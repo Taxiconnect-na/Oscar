edf12604a94eac2493e79ac49a60afd1d3f31c4b
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-08-20 00:38:56 +0200

    Oscar 2.0.008
    [*] Updated start script for codeDeploy.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+(cd /home/ubuntu/Oscar/client/Internal-Dashboards ; rm .env ; mv .env_live .env)
 (cd /home/ubuntu/Oscar/server ; rm .env ; mv .env_live .env)
 (cd /home/ubuntu/Oscar ; pm2 start ecosystem.config.js)
 pm2 save
